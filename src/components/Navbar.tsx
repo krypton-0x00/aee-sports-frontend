90f9988a6b64a768049c227e9fe26b9e559f6dd9
krypton-0x00 <shakirgulzar0x00@gmail.com>
2024-07-30 05:25:29 +0530

    router setup

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,23 +2,35 @@ import { IoHomeOutline } from "react-icons/io5";
 import { IoGameControllerOutline } from "react-icons/io5";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { GoGraph } from "react-icons/go";
+import Link from "next/link";
 
 function Navbar() {
   return (
     <div className="bg-black h-16 flex items-center justify-center ">
       <div className="text-3xl  flex  justify-between items-center px-4 py-2 gap-8 ">
-        <div className="hover:text-highlight transition active:text-white">
-          <IoHomeOutline />
-        </div>
-        <div className="hover:text-highlight transition active:text-white">
-          <IoGameControllerOutline />
-        </div>
-        <div className="hover:text-highlight transition active:text-white">
-          <MdOutlineShoppingCart />
-        </div>
-        <div className="hover:text-highlight transition active:text-white">
-          <GoGraph />
-        </div>
+        <Link href="/">
+          <div className="hover:text-highlight transition active:text-white">
+            <IoHomeOutline />
+          </div>
+        </Link>
+
+        <Link href="/gaming">
+          <div className="hover:text-highlight transition active:text-white">
+            <IoGameControllerOutline />
+          </div>
+        </Link>
+
+        <Link href="/store">
+          <div className="hover:text-highlight transition active:text-white">
+            <MdOutlineShoppingCart />
+          </div>
+        </Link>
+
+        <Link href="/leaderboard">
+          <div className="hover:text-highlight transition active:text-white">
+            <GoGraph />
+          </div>
+        </Link>
       </div>
     </div>
   );
