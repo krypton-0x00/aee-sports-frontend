3d50829c2991a31cabd7fd73da3888cc43e0fb4b
krypton-0x00 <shakirgulzar0x00@gmail.com>
2024-07-30 05:05:24 +0530

    navbar completed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,36 +1,27 @@
- 
 import { IoHomeOutline } from "react-icons/io5";
 import { IoGameControllerOutline } from "react-icons/io5";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { GoGraph } from "react-icons/go";
 
-
- 
 function Navbar() {
   return (
     <div className="bg-black h-16 flex items-center justify-center ">
-      <div className="text-3xl  flex  justify-between items-center px-4 py-2 gap-8">
-
-       <div>
-       <IoHomeOutline />
+      <div className="text-3xl  flex  justify-between items-center px-4 py-2 gap-8 ">
+        <div className="hover:text-highlight transition active:text-white">
+          <IoHomeOutline />
         </div>
-        <div>
-
-       <IoGameControllerOutline />
+        <div className="hover:text-highlight transition active:text-white">
+          <IoGameControllerOutline />
         </div>
-        <div>
-        <MdOutlineShoppingCart />
-
+        <div className="hover:text-highlight transition active:text-white">
+          <MdOutlineShoppingCart />
         </div>
-        <div>
-        <GoGraph />
-
+        <div className="hover:text-highlight transition active:text-white">
+          <GoGraph />
         </div>
       </div>
-
     </div>
-     
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
