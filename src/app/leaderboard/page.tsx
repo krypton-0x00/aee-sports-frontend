bd8047316d0a6f620862f05a56d4926a74293b2e
krypton-0x00 <shakirgulzar0x00@gmail.com>
2024-07-30 20:24:56 +0530

    table

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,12 +1,42 @@
-import SparklesText from "@/components/ui/Sparkles";
-import React from "react";
+import SparklesText from '@/components/ui/Sparkles'
 
-function Leaderboard() {
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/components/ui/Table'
+
+function Leaderboard () {
   return (
-    <div className="flex justify-center flex-col items-center">
-      <SparklesText text="Leaderboard" className="text-4xl py-4" />
+    <div className='flex  justify-center flex-col items-center'>
+      <SparklesText text='Leaderboard' className='text-4xl py-4' />
+      <Table className=''>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className='w-[100px]'>Invoice</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Method</TableHead>
+            <TableHead className='text-right'>Amount</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Method</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className='font-medium'>INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell className='text-right'>$250.00</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </div>
-  );
+  )
 }
 
-export default Leaderboard;
+export default Leaderboard
--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,26 +1,26 @@
-import { InfiniteMovingCards } from "./ui/infinte-cards";
+import { InfiniteMovingCards } from './ui/infinte-cards'
 interface testimonialTypes {
-  quote: string;
-  name: string;
-  title: string;
+  quote: string
+  name: string
+  title: string
 }
 const testimonials: testimonialTypes[] = [
-  { quote: "Mazedar", name: "akash", title: "me hu akash" },
-  { quote: "Bhut badhiya", name: "insane", title: "me hu insane" },
-  { quote: "Are bhai mza aa gya", name: "marco", title: "me hu marco" },
-  { quote: "Are bhai kya baat h ", name: "krypton", title: "me hu krypton" },
-];
-function Reviews() {
+  { quote: 'Mazedar', name: 'akash', title: 'me hu akash' },
+  { quote: 'Bhut badhiya', name: 'insane', title: 'me hu insane' },
+  { quote: 'Are bhai mza aa gya', name: 'marco', title: 'me hu marco' },
+  { quote: 'Are bhai kya baat h ', name: 'krypton', title: 'me hu krypton' }
+]
+function Reviews () {
   return (
-    <div className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+    <div className='h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden'>
       <InfiniteMovingCards
         items={testimonials}
-        direction="right"
-        speed="slow"
+        direction='right'
+        speed='slow'
         pauseOnHover={false}
       />
     </div>
-  );
+  )
 }
 
-export default Reviews;
+export default Reviews
