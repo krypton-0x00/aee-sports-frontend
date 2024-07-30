8333f56cf18ba6a54703886f25eabcd0f525a26a
lakshya <lakshyamogha1@gmail.com>
2024-07-31 04:27:09 +0530

    footer added

---
+++ src/components/Footer.tsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import { FaInstagram } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
+export function Footer() {
+  return (
+    <footer className="py-8 mt-40 border-t-[1px]">
+      <div className="container mx-auto  flex flex-col lg:flex-row justify-between">
+        <div className="flex flex-col lg:flex-row items-center">
+          <Link href="/">
+            <p className="text-2xl font-bold">NxS Esports</p>
+          </Link>
+          <p className=" text-sm mt-2 lg:mt-0">
+            &copy; {2024} NxS Esports. All rights reserved.
+          </p>
+        </div>
+        <nav className="flex flex-col items-center gap-4">
+          <div  className="flex flex-col lg:flex-row items-center">
+            <Link href="/about">
+              <p className=" hover:text-gray-400 text-sm mr-4 lg:mr-8 my-2 lg:my-0">
+                About
+              </p>
+            </Link>
+            <Link href="/contact">
+              <p className=" hover:text-gray-400 text-sm mr-4 lg:mr-8 my-2 lg:my-0">
+                Contact
+              </p>
+            </Link>
+            <Link href="/privacy-policy">
+              <p className=" hover:text-gray-400 text-sm mr-4 lg:mr-8 my-2 lg:my-0">
+                Privacy Policy
+              </p>
+            </Link>
+            <Link href="/terms-of-service">
+              <p className=" hover:text-gray-400 text-sm mr-4 lg:mr-8 my-2 lg:my-0">
+                Terms of Service
+              </p>
+            </Link>
+          </div>
+          <div  className="flex flex-row gap-3 items-center">
+            <Link href="youtube.com">
+            <FaYoutube className="text-2xl hover:text-gray-400" />
+            </Link>
+            <Link href="instagram.com">
+            <FaInstagram className="text-2xl hover:text-gray-400" />
+            </Link>
+            <Link href="whatsapp.com">
+            <FaWhatsapp className="text-2xl hover:text-gray-400" />
+            </Link>
+          </div>
+        </nav>
+      </div>
+    </footer>
+  );
+}
--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,27 +1,27 @@
-import { InfiniteMovingCards } from './ui/infinte-cards'
+import { InfiniteMovingCards } from "./ui/infinte-cards";
 interface testimonialTypes {
-  quote: string
-  name: string
-  title: string
+  quote: string;
+  name: string;
+  title: string;
 }
 const testimonials: testimonialTypes[] = [
-  { quote: 'Mazedar', name: 'akash', title: 'me hu akash' },
-  { quote: 'Bhut badhiya', name: 'insane', title: 'me hu insane' },
-  { quote: 'Are bhai mza aa gya', name: 'marco', title: 'me hu marco' },
-  { quote: 'Are bhai kya baat h ', name: 'krypton', title: 'me hu krypton' }
-]
-function Reviews () {
+  { quote: "Mazedar", name: "akash", title: "me hu akash" },
+  { quote: "Bhut badhiya", name: "insane", title: "me hu insane" },
+  { quote: "Are bhai mza aa gya", name: "marco", title: "me hu marco" },
+  { quote: "Are bhai kya baat h ", name: "krypton", title: "me hu krypton" },
+];
+function Reviews() {
   return (
     <div className="md:h-[30rem] h-[18rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
       <h1 className="text-3xl font-semibold">Reviews</h1>
       <InfiniteMovingCards
         items={testimonials}
-        direction='right'
-        speed='slow'
-        pauseOnHover={false}
+        direction="right"
+        speed="slow"
+        pauseOnHover={true}
       />
     </div>
-  )
+  );
 }
 
-export default Reviews
+export default Reviews;
